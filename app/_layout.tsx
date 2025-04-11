30cce7f8478c06cb67dc9c9e8cc3458d9febe674
Oscar Coronel <ocoronel31@gmail.com>
2025-04-10 22:47:06 -0500

    Comienzo para hacer la calculadora

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,43 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { View } from 'react-native'
+import { Slot, SplashScreen } from 'expo-router'
+import { useFonts } from 'expo-font';
+import { StatusBar } from 'expo-status-bar';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Colors } from '@/constants/Colors';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
+
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
+const RootLayout = () => {
+
+  const insets = useSafeAreaInsets();
+
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf')
   });
 
   useEffect(() => {
-    if (loaded) {
+    if(loaded){
       SplashScreen.hideAsync();
     }
   }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
+  if(!loaded) return null;
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+  return <>
+    <StatusBar style='light' />
+    <View
+      style={{
+        paddingTop: insets.top,
+        backgroundColor: Colors.background,
+        flex: 1
+      }}
+    >
+      <Slot />
+    </View>
+  </>
 }
+
+export default RootLayout
